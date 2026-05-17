061594211b9f155c3d56060eb5f84bcae285829a
FOXesies <n.cryazhev@yandex.ru>
2026-05-17 19:46:46 +0300

    Лабораторная работа 3

--- bye/public/index.php
+++ bye/public/index.php
@@ -5,9 +5,18 @@ require_once __DIR__ . '/../src/Controller/HomeController.php';
 
 $router = new Router();
 
-// Регистрируем маршруты
 $router->add('/welcome', [HomeController::class, 'welcome']);
 $router->add('/bye/{name}', [HomeController::class, 'sayBye']);
+$router->add('/hello', [HomeController::class, 'sayHelloDefault']);
+$router->add('/hello/{username}', [HomeController::class, 'sayHello']);
 
 $currentUri = $_SERVER['REQUEST_URI'];
+
+$currentUri = str_replace('/index.php', '', $currentUri);
+$currentUri = str_replace('/bye/public', '', $currentUri); 
+
+if (empty($currentUri)) {
+    $currentUri = '/';
+}
+
 $router->dispatch($currentUri);
--- bye/src/Controller/HomeController.php
+++ bye/src/Controller/HomeController.php
@@ -2,12 +2,35 @@
 
 class HomeController {
     
+    private function render(string $content, array $variables = []): void {
+        extract($variables);
+
+        include __DIR__ . '/../Views/layout.php';
+    }
+    
     public function welcome(): void {
-        echo "<h1>Добро пожаловать на наш сайт!</h1>";
+        $this->render("<h1>Добро пожаловать на наш сайт!</h1>");
     }
 
     public function sayBye(string $name): void {
         $safeName = htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
-        echo "<h1>Пока, {$safeName}</h1>";
+        
+        $this->render("<h1>Пока, {$safeName}</h1>", [
+            'title' => "Прощание с {$safeName}"
+        ]);
+    }
+
+    public function sayHello(string $username): void {
+        $safeName = htmlspecialchars($username, ENT_QUOTES, 'UTF-8');
+        
+        $this->render("<h1>Приветствуем вас, {$safeName}!</h1>", [
+            'title' => 'Страница приветствия'
+        ]);
+    }
+    
+    public function sayHelloDefault(): void {
+        $this->render("<h1>Приветствуем вас, Гость!</h1>", [
+            'title' => 'Страница приветствия'
+        ]);
     }
 }
